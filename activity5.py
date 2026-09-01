cc1615e115ff377ab66fcf8eacb786f72d240aa0
zedrickbien-dotcom <zedrickbien@gmail.com>
2026-09-01 10:23:06 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,18 @@
+#operators
+#arithmeric
+# + - subtraction
+# * - multiplication
+# / - division
+# // - floor division
+# ** - exponentation
+# % - modulus
+
+a = 155
+b = 57
+sum = a + b
+diff = a - b
+product = a * b
+
+print( the sum of ",a," and ",b," is,sum)
+print("the difference of ",a," and ",b," is,diff)
+print("the product of ",a," and ",b," is,a*b)
