654766e9d10549618e62d7b366c8cbcd31c8b60d
zedrickbien-dotcom <zedrickbien@gmail.com>
2026-09-10 16:35:11 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,19 @@
+name = input("input name -----> ")
+age = int(input("input age -----> "))
+print("hi,",name,"your age is considered as an")
+
+if age >= 1 and age <= 5:
+            print("infant")
+else:
+            print("invalid")
+
+if age >= 6 and age <= 12:
+            print("kid")
+else:
+            print("invalid")
+
+
+if age >= 13 and age <= 19:
+            print("teen")
+else:
+            print("invalid")
